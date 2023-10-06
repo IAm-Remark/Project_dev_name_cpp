a6497da7eb3917a2e51c57fccecefc36f14d412c
IAm_Remark <gangamstul4@gmail.com>
2023-10-06 16:08:57 +0300

    Self_project

---
+++ cpp_project/cpp_project/cpp_project.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int SizeXI = 15;
+const int SizeYI = 15;
+int MatrixI[SizeXI][SizeYI];
+
+void FieldMatrixC(int Matrix[SizeXI][SizeYI])
+{
+    for (int i = 0; i <= SizeXI; i++)
+    {
+        for (int j = 0; j <= SizeYI; j++)
+        {
+            Matrix[i][j] = j;
+            //cout << Matrix[i][j];
+        }
+        //cout << "\n";
+    }
+}
+
+
+int main()
+{
+    
+    FieldMatrixC(MatrixI);
+    
+    
+
+}
+
